3cee165d4d403fc7eb5180676a4a5ac42fab4040
gabriel-vieira-chagas <gabrielvieirachagas@gmail.com>
2025-04-04 04:29:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string meuNome = "Gabriel Vieira Chagas"; //Cria variável com meu nome
+        string dataNascimento = "20/06/2005"; //Cria variável com minha data de nascimento
+        Console.WriteLine($"Olá, meu nome é {meuNome}!"); //Exibe a frase requisitada
+        Console.WriteLine($"Nasci em {dataNascimento} e estou aprendendo C#!"); //Exibe a frase requisitada
+    }
+}
